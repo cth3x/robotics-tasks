9ae4c4e664516aa0b6b3d60c064f6f8361a3e0df
cth2x <colehicks50@gmail.com>
2024-07-18 20:59:32 -0500

    Code updated

--- task-2/drive2.c
+++ task-2/drive2.c
@@ -1,41 +1,41 @@
-#include <kipr/wombat.h>
+// #include <kipr/wombat.h>
 
 int main()
 {
     create_connect();
-    printf("Create is connected\n");
 
-    int lcliff = 0;
-    int rcliff = 0;
+    int lcliff;
+    int rcliff;
 
-    int lfcliff = 0;
-    int rfcliff = 0;
-
-    lcliff = get_create_lcliff_amt();
-    lfcliff = get_create_lfcliff_amt();
-    rcliff = get_create_rcliff_amt();
-    rfcliff = get_create_rfcliff_amt();
+    int lfcliff;
+    int rfcliff;
 
     while (1)
     {
+        lcliff = get_create_lcliff_amt();
         lfcliff = get_create_lfcliff_amt();
+        rcliff = get_create_rcliff_amt();
         rfcliff = get_create_rfcliff_amt();
 
-        if (lfcliff < 2700 && rfcliff < 2700)
+        if (lcliff > 2700 && rcliff > 2700)
         {
-            create_drive_direct(200, 200);
+            create_drive_direct(0, 0);
         }
-
-        if (lfcliff > 2700 && rfcliff < 2700)
+        else if (lfcliff < 2700 && rfcliff < 2700)
         {
-            create_drive_direct(-60, 100);
+            create_drive_direct(250, 250);
         }
-
+        else if (lfcliff > 2700 && rfcliff < 2700)
+        {
+            create_drive_direct(-100, 200);
+        }
         else if (lfcliff < 2700 && rfcliff > 2700)
         {
-            create_drive_direct(100, -60);
+            create_drive_direct(200, -100);
         }
     }
+
+    create_stop();
 
     create_disconnect();
 
